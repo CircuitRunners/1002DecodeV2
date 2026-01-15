62bc307c0ee3000f153e051cfcd63a192c1bef0a
Connor Reddington <99690237+CWRedd@users.noreply.github.com>
2026-01-14 21:48:58 -0500

    using turret encoder for tuner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testers/notCookedTurretTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testers/notCookedTurretTuner.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.Testers;
 //import com.acmerobotics.dashboard.FtcDashboard;
 //import com.acmerobotics.dashboard.config.Config;
 //import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.bylazar.configurables.annotations.Configurable;
 import com.bylazar.telemetry.PanelsTelemetry;
 import com.bylazar.telemetry.TelemetryManager;
@@ -17,6 +19,7 @@ import com.qualcomm.robotcore.util.Range;
 import com.seattlesolvers.solverslib.controller.PIDFController;
 
 import org.firstinspires.ftc.teamcode.Config.Subsystems.Sensors;
+import org.firstinspires.ftc.teamcode.Config.Subsystems.Shooter;
 
 import java.util.List;
 
@@ -41,6 +44,8 @@ public class notCookedTurretTuner extends OpMode {
     private Sensors sensors = new Sensors();
     private ElapsedTime loopTimer = new ElapsedTime();
 
+    private Shooter shooter;
+
     public static double cookedLoopTargetMS = 40;
    // private TelemetryManager panelsTelemetry;
     private static final String HUB_NAME = "SRSHub";
@@ -50,7 +55,7 @@ public class notCookedTurretTuner extends OpMode {
         // Initialize Panels Telemetry system
        // panelsTelemetry = PanelsTelemetry.INSTANCE.getTelemetry();
 
-
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
         // 1. Bulk Read Setup for faster loop times
         List<LynxModule> hubs = hardwareMap.getAll(LynxModule.class);
         for (LynxModule hub : hubs) {
@@ -66,6 +71,8 @@ public class notCookedTurretTuner extends OpMode {
         // 3. Direction logic based on the Dashboard toggle
         turret.setDirection(isReversed ? DcMotorSimple.Direction.REVERSE : DcMotorSimple.Direction.FORWARD);
 
+
+        shooter = new Shooter(hardwareMap,telemetry);
         // 4. Sensor Initialization
         try {
             sensors.init(hardwareMap, HUB_NAME);
@@ -90,8 +97,8 @@ public class notCookedTurretTuner extends OpMode {
 
         // Update sensor data
         sensors.update();
-        double currentTicks = turret.getCurrentPosition();
-        double currentAngle = ((currentTicks / ticksPerRevolution) / gearRatio) * 360;
+//        double currentTicks = turret.getCurrentPosition();
+//        double currentAngle = ((currentTicks / ticksPerRevolution) / gearRatio) * 360;
 
 
         // Update PID coefficients and target live from Dashboard
@@ -99,17 +106,17 @@ public class notCookedTurretTuner extends OpMode {
         pidf.setSetPoint(targetAngle);
 
         // Calculate and apply power
-        double turretOutput = pidf.calculate(currentAngle);
+        double turretOutput = pidf.calculate(shooter.getCurrentTurretPosition());
         turretOutput = Range.clip(turretOutput, -maxPower, maxPower);
         turret.setPower(turretOutput);
 
-        double error = targetAngle - currentAngle;
+        //double error = targetAngle - currentAngle;
 
         // --- STANDARD TELEMETRY (Driver Station Phone) ---
         telemetry.addData("--- TURRET TUNING ---", "");
         telemetry.addData("Target", targetAngle);
-        telemetry.addData("Actual", "%.2f", currentAngle);
-        telemetry.addData("Error", "%.2f", error);
+        telemetry.addData("Actual", "%.2f", shooter.getCurrentTurretPosition());
+        //telemetry.addData("Error", "%.2f", error);
         telemetry.addData("Power", "%.2f", turretOutput);
         double loopTime = loopTimer.milliseconds();
 
